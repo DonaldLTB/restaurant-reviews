d0d01f4600aafd82bdbd07e164c2d845a2a69b6a
Lawrence Kroll <larry43.lk@gmail.com>
2021-01-12 16:53:32 +0900

    reviews with destroy method

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,15 @@ Rails.application.routes.draw do
       # get 'top', to: 'restaurants#top', as: :top_restaurants
       get :top
     end
+
+    member do
+      get :chef
+    end
+    resources :reviews, only: [:new, :create]
   end
+
+  resources :reviews, only: [:destroy] # why outside, because you just wanna destroy reviews so
+  # shouldnt have sth to do with restaurants (id)
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

e38f803b379ac2448199782614ee9036564f9131
Lawrence Kroll <larry43.lk@gmail.com>
2021-01-12 15:43:31 +0900

    chef added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  resources :restaurants
+  # collection
+  # members
+  resources :restaurants do
+    collection do
+      # get 'top', to: 'restaurants#top', as: :top_restaurants
+      get :top
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
